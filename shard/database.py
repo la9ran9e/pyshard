ef3cbe97b7cde64e88810a2eb7396036e4d65be9
tvauri <t.tvauri@rambler-co.ru>
2019-04-24 13:27:12 +0300

    just added some code

--- shard/database.py
+++ shard/database.py
@@ -1,4 +1,6 @@
 import sys
+from collections import defaultdict
+
 
 class Table(object):
 	def __init__(self, storage):
@@ -19,18 +21,55 @@ def _sort_shards(shards):
 
 
 class Shard:
-	def __init__(self, max_size=1024):
+	def __init__(self, start=None, end=None, max_size=1024, bins_num=5):
 		self._empty = True
 		self.storage = dict()
 		self.size = 0
 		self.max_size = max_size
+		self._start = start
+		self._end = end
 
+		self._bins_num = bins_num
+		self._bin_step = self.estimate_bin_step()
+
+		self.distr = defaultdict(int)
+
+	@property
+	def start(self):
+		return self._start
+	
+	@property
+	def end(self):
+		return self._end
+
+	def estimate_bin_step(self):
+		if self._start is not None and self._end is not None:
+			self._bin_step = (self._end-self._start)/self._bins_num
+		else:
+			self._bin_step = None
+
+		return self._bin_step
+	
+	@start.setter
+	def start(self, value):
+		self._start = value
+
+	@end.setter
+	def end(self, value):
+		self._end = value
+
+	def _get_bin(self, hash):
+		steps = (hash - self._start)//self._bin_step
+		bin_ = self._start + (self._bin_step*steps)
+
+		return bin_
+	
 	@property
 	def empty(self):
 		return len(self.storage) == 0
 
 	@property
-	def left(self):
+	def free_mem(self):
 		return self.max_size - self.size
 	
 	def write(self, key, hash, record):
@@ -47,6 +86,9 @@ class Shard:
 
 		self.size += item_size
 
+		bin_ = self._get_bin(hash)
+		self.distr[bin_] += 1
+
 		return item_size
 
 	def read(self, key):
@@ -57,6 +99,10 @@ class Shard:
 		item_size = _get_size(record)
 		self.size -= item_size
 
+		hash = record['hash']
+		bin_ = self._get_bin(hash)
+		self.distr[bin_] -= 1
+
 		return record
 
 	def remove(self, key):
@@ -66,6 +112,9 @@ class Shard:
 
 		item_size = _get_size(record)
 		self.size -= item_size
+
+		bin_ = self._get_bin(hash)
+		self.distr[bin_] -= 1
 
 		return item_size
 
@@ -106,46 +155,73 @@ def _make_bins(num):
 class Master:
 	def __init__(self, shards_num=2, method='md5'):
 		self._method = method
+		self._bin_step = 1/shards_num
 		self._bins = _make_bins(shards_num)
 		self._shards = self._make_shards()
 
 	def _make_shards(self, size=1024):
-		return {bin_: Shard(max_size=size) for bin_ in self._bins}
+		bins = dict()
 
-	def _get_bin_id(self, key):
+		for bin_ in self._bins:
+			next_bin = bin_+self._bin_step
+			bins[bin_] = Shard(start=bin_, end=next_bin, max_size=size)
+		
+		return bins
+
+	def _get_bin(self, key):
 		id_ = hash_key(key, self._method, 1e7)
 		i = bisect.bisect_left(self._bins, id_)-1
 
 		return self._bins[i], id_
 
 	def get_shard(self, key):
-		id_, hash_key_ = self._get_bin_id(key)
-		return hash_key_, self._shards[id_]
+		bin_, hash_key_ = self._get_bin(key)
+		return hash_key_, self._shards[bin_]
 
-	def split(self, increase_num):
-		self._bins = _make_bins(len(self._shards)+increase_num)
+	def split(self, increase_num, size=1024):
+		num = len(self._shards)+increase_num
+		self._bins = _make_bins(num)
 		old_shards = self._shards
-		self._shards = self._make_shards()
+		self._bin_step = 1/num
+		self._shards = self._make_shards(size=size)
 		for shard in old_shards.values():
 			self._do_distr(shard)
 
-	def _do_distr(self, shard):
-		for key, value in shard.items():
-			id_ = value['hash']
-			i = bisect.bisect_left(self._bins, id_)-1
-			bin_ = self._bins[i]
+	def _do_distr(self, shard, target=None):
+		if not target:
+			for key, value in shard.items():
+				id_ = value['hash']
+				i = bisect.bisect_left(self._bins, id_)-1
+				bin_ = self._bins[i]
 
-			self._shards[bin_].write(key, **value)
+				self._shards[bin_].write(key, **value)
+		else:
+			for key, value in shard.items():
+				target.write(key, **value)
 
-	def insert_right(self, bin_, size=1024):
+	def insert(self, bin_, size=1024):
 		i = bisect.bisect_right(self._bins, bin_)
 		left_bin = self._bins[i-1]
+		right_bin = self._bins[i+1]
 		self._bins[i:i] = [bin_]
-		self._shards[bin_] = Shard(max_size=size)
-		_to_move = [key for key, value in self._shards[left_bin].items() if value['hash'] >= bin_]
+		new_shard = Shard(start=bin_, end=right_bin, max_size=size)
+		left_shard = self._shards[left_bin]
+
+		_to_move = [key for key, value in left_shard.items() if value['hash'] >= bin_]
 		for key in _to_move:
-			item = self._shards[left_bin].pop(key)
-			self._shards[bin_].write(key, **item)
+			item = left_shard.pop(key)
+			new_shard.write(key, **item)
+
+		self._shards[bin_] = new_shard
+
+	def remove(self, bin_):
+		i = self._bins.index(bin_)
+		del self._bins[i]
+		tmp = self._shards[bin_]
+		del self._shards[bin_]
+		left_bin = self._bins[i-1]
+		target = self._shards[left_bin]
+		self._do_distr(tmp, target)
 
 	def rebalance(self, shard):
 		...
@@ -157,7 +233,9 @@ class Master:
 		for shard_id, shard in sorted(self._shards.items()):
 			stat[shard_id] = {
 				'total memory': shard.max_size,
-				'free memory': shard.left
+				'free memory': shard.free_mem,
+				'distribution': dict(shard.distr)
+
 			}
 
 		return stat
@@ -185,6 +263,9 @@ class App:
 		return getattr(self._master, attr)
 	
 
+import pprint
+
+pp = pprint.PrettyPrinter()
 
 master = Master()
 app = App(master)
@@ -199,14 +280,12 @@ app.write('name5', {'b': 'b'})
 app.write('name6', {'b': 'b'})
 app.write('name7', {'b': 'b'})
 app.write('name8', {'b': 'b'})
-print(app.stat)
-for id_, shard in master._shards.items():
-	print(id_, shard.storage)
+pp.pprint(app.stat)
 app.split(2)
-print(app.stat)
-for id_, shard in master._shards.items():
-	print(id_, shard.storage)
-app.insert_right(.2)
-print(app.stat)
-for id_, shard in master._shards.items():
-	print(id_, shard.storage)
+pp.pprint(app.stat)
+app.insert(.2)
+pp.pprint(app.stat)
+app.remove(.2)
+pp.pprint(app.stat)
+
+
