e25fbd1f5b5cff5506e398502499a7b72f16694b
la9ran9e <tvauritimur@gmail.com>
2019-07-19 01:08:46 +0300

    + remove trash

--- pyshard/shard/shard.py
+++ pyshard/shard/shard.py
@@ -82,7 +82,6 @@ class Shard:
         doc = {'hash_': hash_, 'record': record}
 
         offset = self.storage.write(index, key, doc)
-        print(offset)
         if offset == 0:  # TODO replace memory control to storage
             return 0
 
