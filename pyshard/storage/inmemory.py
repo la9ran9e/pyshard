c0615b3d900c7582dda514fe19bb64d440a024ff
la9ran9e <tvauritimur@gmail.com>
2019-07-18 21:01:23 +0300

    + indexes feature integration

--- pyshard/storage/inmemory.py
+++ pyshard/storage/inmemory.py
@@ -1,5 +1,5 @@
 from .base import BaseStorage
-from .errors import IndexNotFoundError
+from .errors import IndexNotFoundError, IndexExistsError
 
 
 class InMemoryStorage(BaseStorage):
@@ -16,6 +16,8 @@ class InMemoryStorage(BaseStorage):
 
     def write(self, index, key, record):
         collection = self._get_index(index)
+        if key in collection:
+            return 0
         collection[key] = record
 
     def pop(self, index, key):
@@ -27,10 +29,29 @@ class InMemoryStorage(BaseStorage):
         del collection[key]
 
     def create_index(self, index):
+        if index in self._storage:
+            raise IndexExistsError(index)
         self._storage[index] = dict()
 
     def drop_index(self, index):
         del self._storage[index]
+
+    def values(self):
+        for index in self.indexes:
+            for value in self.index_values(index):
+                yield value
+
+    def index_values(self, index):
+        collection = self._get_index(index)
+        for key in collection:
+            yield collection[key]
+
+    @property
+    def empty(self):
+        for index in self.indexes:
+            if self._storage[index]:
+                return False
+        return True
 
     def _get_index(self, index):
         if index not in self._storage:
