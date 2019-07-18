0029348f389506263e267c017035461728454c57
tvauri <t.tvauri@rambler-co.ru>
2019-07-18 12:25:16 +0300

    + add long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyshard',
     version='0.1',
     description='Distributed key-value storage in Python stdlib',
+    long_description=long_description,
     author='la9ran9e',
+    author_email="tvauritimur@gmail.com",
     uri='https://github.com/la9ran9e/pyshard',
     packages=[
         'pyshard',
