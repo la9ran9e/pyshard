7343ace7795366ba4c9d736019993f77d9f79716
la9ran9e <tvauritimur@gmail.com>
2019-07-19 02:03:11 +0300

    + v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyshard',
-    version='0.0.2',
+    version='0.1.0',
     description='Distributed key-value storage in Python stdlib',
     long_description=long_description,
     long_description_content_type="text/markdown",
